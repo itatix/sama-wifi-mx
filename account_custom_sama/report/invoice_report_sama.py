493eb771fe391e637c495d32febf8ee5b8c58daf
Antonio Silva <gsisa.asilva@gmail.com>
2021-06-16 06:59:18 -0500

    [FIX]account_custom_sama: FIX months names

--- account_custom_sama/report/invoice_report_sama.py
+++ account_custom_sama/report/invoice_report_sama.py
@@ -17,6 +17,18 @@ months = {
         'OCTUBRE':10,
         'NOVIEMBRE':11,
         'DICIEMBRE':12,
+        'JANUARY':1,
+        'FEBRUARY':2,
+        'MARCH':3,
+        'APRIL':4,
+        'MAY':5,
+        'JUNE':6,
+        'JULY':7,
+        'AUGUST':8,
+        'SEPTEMBER':9,
+        'OCTOBER':10,
+        'NOVEMBER':11,
+        'DECEMBER':12,
     }
 
 class InvoiceReportSama(models.Model):
